ff9d7c771da6c6a7dceb43e4f5130160fb286e92
lraffucci <lraffucci@gmail.com>
2023-03-14 10:13:42 -0500

    updated message from server

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.json({ message: "Hello from server using nodeJS 18" });
 });
 
 
